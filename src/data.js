601f25294206e7592cec9ba5060241f068f528c5
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-16 15:56:02 -0500

    getting familiar with project app

--- src/data.js
+++ src/data.js
@@ -26,5 +26,33 @@ export default [
 		price: 40.00,
 		image:
 			'https://images-na.ssl-images-amazon.com/images/I/61uUPXbhMxL._SX397_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 5,
+		title: 'War and Peace',
+		price: 10.00,
+		image:
+			'https://pictures.abebooks.com/isbn/9780060798871-us.jpg'
+	},
+	{
+		id: 6,
+		title: 'Napoleon: A Life',
+		price: 10.00,
+		image:
+			'https://images-na.ssl-images-amazon.com/images/I/51MQqw2ffHL._SX331_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 7,
+		title: 'Moby Dick',
+		price: 10.00,
+		image:
+			'https://images-na.ssl-images-amazon.com/images/I/51xJEYQksZL._SX331_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 8,
+		title: 'Thus Spake Zarathustra',
+		price: 10.00,
+		image:
+			'https://images-na.ssl-images-amazon.com/images/I/4177g1L0NOL._SX322_BO1,204,203,200_.jpg'
 	}
 ];
